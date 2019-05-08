049f13f6dc70d7163b12c8d3d7c37a8038999685
Jain, Amit <amitjain.developer@gmail.com>
2019-05-08 12:47:08 -0500

    initial commit

---
+++ amitproxy-proxy/src/main/java/com/amitjain/amitproxyproxy/AmitproxyProxyApplication.java
@@ -0,0 +1,13 @@
+package com.amitjain.amitproxyproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmitproxyProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmitproxyProxyApplication.class, args);
+	}
+
+}
---
+++ amitproxy-proxy/src/test/java/com/amitjain/amitproxyproxy/AmitproxyProxyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.amitjain.amitproxyproxy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmitproxyProxyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
